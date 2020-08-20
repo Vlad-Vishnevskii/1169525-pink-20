4674ca088cff92f464ce98dc70bea04d246d61b8
Vlad <vishnevskii_vlad@mail.ru>
2020-08-20 16:53:26 +0300

    правки по меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,13 +2,8 @@ var navMain = document.querySelector('.main-nav');
 var navButtonOpen = document.querySelector('.main-nav__burger-button');
 var navButtonClose = document.querySelector('.main-nav__close-button');
 
-
 navButtonOpen.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav_closed')) {
-    navMain.classList.remove('main-nav_closed');
-  } else {
-    navMain.classList.add('main-nav--closed');
-  }
+  navMain.classList.remove('main-nav_closed');
 });
 
 navButtonClose.addEventListener('click', function() {
