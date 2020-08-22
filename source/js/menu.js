ef826e16815bef45fcf9160f793aad4a365db239
Vlad <vishnevskii_vlad@mail.ru>
2020-08-22 23:20:19 +0300

    Меню при нерабатающем js, подключение локальных шрифтов

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,8 @@ var navMain = document.querySelector('.main-nav');
 var navButtonOpen = document.querySelector('.main-nav__burger-button');
 var navButtonClose = document.querySelector('.main-nav__close-button');
 
+navMain.classList.remove('main-nav_nojs');
+
 navButtonOpen.addEventListener('click', function() {
   navMain.classList.remove('main-nav_closed');
 });
