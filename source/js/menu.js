ef6a28693c0d3f6761f5ba1db355b57938bff278
Vlad <vishnevskii_vlad@mail.ru>
2020-08-29 23:18:15 +0300

    исправление Б2 Б5 Б32 Б25 Б27 Б28

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,13 @@
-var navMain = document.querySelector('.main-nav');
-var navButtonOpen = document.querySelector('.main-nav__burger-button');
-var navButtonClose = document.querySelector('.main-nav__close-button');
+var navMain = document.querySelector(".main-nav");
+var navButtonOpen = document.querySelector(".main-nav__burger-button");
+var navButtonClose = document.querySelector(".main-nav__close-button");
 
-navMain.classList.remove('main-nav_nojs');
+navMain.classList.remove("main-nav_nojs");
 
-navButtonOpen.addEventListener('click', function() {
-  navMain.classList.remove('main-nav_closed');
+navButtonOpen.addEventListener("click", function() {
+  navMain.classList.remove("main-nav_closed");
 });
 
-navButtonClose.addEventListener('click', function() {
-  navMain.classList.add('main-nav_closed');
+navButtonClose.addEventListener("click", function() {
+  navMain.classList.add("main-nav_closed");
 });
